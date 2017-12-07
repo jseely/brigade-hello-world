ac51b98abc34148f153aefe100db8ab5e31185f8
Justin Seely <justin.seely@gmail.com>
2017-12-07 01:22:11 +0000

    Add push event

--- brigade.js
+++ brigade.js
@@ -3,3 +3,7 @@ const { events } = require("brigadier")
 events.on("exec", () => {
   console.log("==> handling an 'exec' event")
 })
+
+events.on("push", () => {
+  console.log("Someone pushed to github!")
+})
